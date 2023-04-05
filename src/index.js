497c9fd742f27059c29da8e623669e25d1fd8f2f
ibrahim <ihussein292@gmail.com>
2023-04-05 14:17:23 +0300

    Completed Assingment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const tasksList = document.getElementById('tasks');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const taskInput = document.getElementById('new-task-description');
+    const taskDescription = taskInput.value.trim();
+    if (taskDescription !== '') {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskDescription;
+      tasksList.appendChild(taskItem);
+      taskInput.value = '';
+    }
+  });
 });
